05a1d97ae489c5f11ef465206f2cb6f2f8a70b10
chengfeng30121 <chengfeng3012@163.com>
2024-08-31 12:30:03 +0000

    modified:   init_build.py

--- init_build.py
+++ init_build.py
@@ -1,2 +1,6 @@
+import os
+
+os.makedirs("/home/runner/.buildozer/android/platform/android-sdk/licenses/", exist_ok=True)
+
 with open("/home/runner/.buildozer/android/platform/android-sdk/licenses/android-sdk-license", "w") as f:
     f.write("\n24333f8a63b6825ea9c5514f83c2829b004d1fee")
