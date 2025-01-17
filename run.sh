2ffb6d3d4922a6b1cca014056deeb55644ef4eef
chengfeng30121 <chengfeng3012@163.com>
2025-01-17 22:10:00 +0800

    Action test

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
-sudo echo "Types: deb\nURIs: http://mirrors.aliyun.com/ubuntu/\nSuites: noble noble-updates noble-security\nComponents: main restricted universe multiverse\nSigned-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg\n" > /etc/apt/sources.list.d/ubuntu.sources
+sudo sh -c 'echo "Types: deb
+URIs: http://mirrors.aliyun.com/ubuntu/
+Suites: noble noble-updates noble-security
+Components: main restricted universe multiverse
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
+" > /etc/apt/sources.list.d/ubuntu.sources'
 sudo apt update
 sudo apt install libltdl7-dev
