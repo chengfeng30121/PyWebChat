937f7c35c8c4601b6d3d383828f2fa0e62579506
chengfeng30121 <chengfeng3012@163.com>
2025-01-17 22:12:11 +0800

    Action test

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 sudo sh -c 'echo "Types: deb
-URIs: http://mirrors.aliyun.com/ubuntu/
-Suites: noble noble-updates noble-security
+URIs: https://mirrors.tuna.tsinghua.edu.cn/ubuntu
+Suites: noble noble-updates noble-backports
+Components: main restricted universe multiverse
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
+
+Types: deb
+URIs: http://security.ubuntu.com/ubuntu/
+Suites: noble-security
 Components: main restricted universe multiverse
 Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
 " > /etc/apt/sources.list.d/ubuntu.sources'
