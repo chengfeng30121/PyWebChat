7100ff722d5eca412ace3f149a678b663d42c068
chengfeng30121 <chengfeng3012@163.com>
2025-01-17 22:00:42 +0800

    Action test

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
-lsb_release -a
-echo "> cat /etc/apt/sources.list.d/ubuntu.sources"
-cat /etc/apt/sources.list.d/ubuntu.sources
+cat "Types: deb
+URIs: http://mirrors.aliyun.com/ubuntu/
+Suites: noble noble-updates noble-security
+Components: main restricted universe multiverse
+Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
+" > /etc/apt/sources.list.d/ubuntu.sources
+sudo apt update
+sudo apt install libltdl7-dev
